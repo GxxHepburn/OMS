2dba384981b48926d2c19cc514e31e7ca50716b1
gxx <1152862327@qq.com>
2021-01-12 19:35:01 +0800

    add backtotop

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -43,6 +43,26 @@
         <router-view></router-view>
       </el-main>
     </el-container>
+    <el-backtop bottom="100">
+      <div
+        style="{
+          height: 100%;
+          width: 100%;
+          background-color: #f2f5f6;
+          box-shadow: 0 0 6px rgba(0,0,0, .12);
+          text-align: center;
+          line-height: 40px;
+          color: #1989fa;
+        }"
+      >
+        UP
+      </div>
+    </el-backtop>
+    <el-backtop>
+      <div>
+        <i class="el-icon-caret-top"></i>
+      </div>
+    </el-backtop>
   </el-container>
 </template>
 
