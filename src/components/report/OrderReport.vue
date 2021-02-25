cb1f6fc42cabc646d3c89d0a94ada40389e76751
gxx <1152862327@qq.com>
2021-02-26 00:23:20 +0800

    完成订单数PHour

--- src/components/report/OrderReport.vue
+++ src/components/report/OrderReport.vue
@@ -21,11 +21,36 @@
                   type="date"
                   :editable="false"
                   :clearable="false"
-                  placeholder="选择日期"
-                  @change="initToday(todayPicker)">
+                  placeholder="选择日期">
                 </el-date-picker>
-                <el-button style="margin-left:30px;" type="primary">搜索</el-button>
+                <el-button style="margin-left:30px;" type="primary" @click="searchOrdersPHour">搜索</el-button>
             </div>
+            <el-table :data='hourFormList'
+              :border="true"
+             :stripe="true" v-if="hourFormList.length > 0">
+             <el-table-column label="时段">
+               <template slot-scope="scope">
+                 {{scope.row.hours}}
+               </template>
+             </el-table-column>
+             <el-table-column label="消费总额">
+               <template slot-scope="scope">
+                 {{scope.row.totalPrice.toFixed(2)}}
+               </template>
+             </el-table-column>
+             <el-table-column label="订单数" prop="totalOrdersNumbers"></el-table-column>
+             <el-table-column label="客人数" prop="numberOfDinners"></el-table-column>
+             <el-table-column label="单均">
+               <template slot-scope="scope">
+                 {{scope.row.pricePOrder.toFixed(2)}}
+               </template>
+             </el-table-column>
+             <el-table-column label="人均">
+               <template slot-scope="scope">
+                 {{scope.row.pricePPeople.toFixed(2)}}
+               </template>
+             </el-table-column>
+            </el-table>
           </el-tab-pane>
           <el-tab-pane label="订单数(日)" name="ordersPDay">
           </el-tab-pane>
@@ -43,14 +68,26 @@ export default {
     return {
       tabsValue: 'ordersPHour',
       today: '',
-      todayPicker: ''
-
+      todayPicker: '',
+      hourFormList: []
     }
   },
   created () {
     this.initToday(new Date())
   },
   methods: {
+    // 搜索订单数(h)
+    async searchOrdersPHour () {
+      if (this.todayPicker !== '') {
+        this.initToday(this.todayPicker)
+      }
+      const { data: res } = await this.$http.post('searchOrdersPHour', { today: this.today, mmngctUserName: window.sessionStorage.getItem('mmngctUserName') })
+      if (res.meta.status !== 200) {
+        this.$message.error('获取订单数(h)统计数据失败!')
+        return
+      }
+      this.hourFormList = res.data.hourFormList
+    },
     initToday (todayDate) {
       var day = todayDate.getDate() <= 9 ? '0' + todayDate.getDate() : todayDate.getDate()
       var month = todayDate.getMonth() <= 9 ? '0' + (todayDate.getMonth() + 1) : todayDate.getMonth() + 1
@@ -65,5 +102,10 @@ export default {
 .statistics_time {
   font-weight: lighter;
   font-size: 13px;
+}
+/deep/ th {
+  text-align: center;
+  background-color: #FFEEFF;
+  border-color: #CCCCCC;
 }
 </style>
