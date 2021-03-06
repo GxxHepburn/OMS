45b5dae8e239f77f9acd6631ef7f5c8140cf8031
gxx <1152862327@qq.com>
2021-03-07 02:58:28 +0800

    完成翻台率TR

--- src/components/report/TabsReport.vue
+++ src/components/report/TabsReport.vue
@@ -70,10 +70,34 @@
                 type="date"
                 :editable="false"
                 :clearable="false"
-                placeholder="选择年份">
+                placeholder="选择日期">
               </el-date-picker>
               <el-button style="margin-left:30px;" type="primary" @click="searchTRFormList">搜索</el-button>
             </div>
+            <el-table :data='TRFormList'
+              :border='true'
+              :stripe="true">
+              <el-table-column label="桌台数" prop="tabnum"></el-table-column>
+              <el-table-column label="餐位数" prop="tabPersonNum"></el-table-column>
+              <el-table-column label="交易数" prop="tradeNum"></el-table-column>
+              <el-table-column label="开台数" prop="openingNum"></el-table-column>
+              <el-table-column label="客人数" prop="numberOfDiners"></el-table-column>
+              <el-table-column label="上座率" prop="attendance">
+                <template slot-scope="scope">
+                  {{(scope.row.attendance*100).toFixed(2) + '%'}}
+                </template>
+              </el-table-column>
+              <el-table-column label="开台率" prop="openingRate">
+                <template slot-scope="scope">
+                  {{(scope.row.openingRate*100).toFixed(2) + '%'}}
+                </template>
+              </el-table-column>
+              <el-table-column label="翻台率" prop="turnoverRate">
+                <template slot-scope="scope">
+                  {{(scope.row.turnoverRate*100).toFixed(2) + '%'}}
+                </template>
+              </el-table-column>
+            </el-table>
           </el-tab-pane>
         </el-tabs>
     </div>
